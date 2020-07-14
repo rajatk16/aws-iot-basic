fd266902dc46d396ca6f9eeb00c345bea8f90cf8
Rajat S <rajat.sudagade@gmail.com>
2020-07-14 14:54:02 -0500

    basic Expo_RN App with AWS IoT

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import './shim'
+import AWS from 'aws-sdk'
+import AWSIoTData from 'aws-iot-device-sdk'
+import { Text, View } from 'react-native';
+import React from 'react'
 
-export default function App() {
+const awsConfig = {
+  identityPoolId: 'us-east-1:e98e0875-957d-4eb8-9188-036b12aa85ec',
+  mqttEndpoint: `a5jlh5mgikrwc-ats.iot.us-east-1.amazonaws.com`,
+  region: 'us-east-1',
+  clientId: '3mda5opn49lsutvsbu390vmt12',
+  userPoolId: 'us-east-1_Hg857HnGJ'
+}
+console.log(awsConfig)
+
+const mqttClient = AWSIoTData.device({
+  region: awsConfig.region,
+  host: awsConfig.mqttEndpoint,
+  clientId: awsConfig.clientId,
+  protocol: 'wss',
+  // maximumReconnectTimeMs: 5000,
+  debug: false,
+  accessKeyId: '',
+  secretKey: '',
+  sessionToken: '',
+  isTrusted: true
+})
+
+AWS.config.region = awsConfig.region;
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: awsConfig.identityPoolId
+})
+
+AWS.config.credentials.get((err) => {
+  if (err) {
+    console.log(AWS.config.credentials);
+    throw err;
+  } else {
+    mqttClient.updateWebSocketCredentials(
+      AWS.config.credentials.accessKeyId,
+      AWS.config.credentials.secretAccessKey,
+      AWS.config.credentials.sessionToken
+    )
+  }
+})
+
+mqttClient.on('connect', () => {
+  console.log('Connected To IoT Client')
+  mqttClient.subscribe('real-time-weather')
+})
+
+mqttClient.on('error', (err) => {
+  console.log(err)
+})
+
+mqttClient.on('message', (topic, payload) => {
+  const msg = JSON.parse(payload.toString());
+  console.log(msg)
+})
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        RealTime Weather
+      </Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
